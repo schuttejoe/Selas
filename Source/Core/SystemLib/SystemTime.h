c8ea49d715b570022f9d38d3c328704fa77e0b06
Joe Schutte <schutte.joe@gmail.com>
2018-07-01 21:12:18 -0700

    Timer utility.

--- Source/Core/SystemLib/SystemTime.h
+++ Source/Core/SystemLib/SystemTime.h
@@ -20,4 +20,35 @@ namespace Selas
         float ElapsedMillisecondsF(std::chrono::high_resolution_clock::time_point& since);
         float ElapsedSecondsF(std::chrono::high_resolution_clock::time_point& since);
     }
+
+    // std::micro, std::milli, etc...
+    template<typename Rate_>
+    class CScopedTimeAccumulator
+    {
+    private:
+        std::chrono::high_resolution_clock::time_point startTime;
+        std::chrono::duration<float, Rate_>* destination;
+
+    public:
+        CScopedTimeAccumulator(std::chrono::duration<float, Rate_>* accumulator);
+        ~CScopedTimeAccumulator();
+    };
+
+    //==============================================================================
+    template <typename Rate_>
+    CScopedTimeAccumulator<Rate_>::CScopedTimeAccumulator(std::chrono::duration<float, Rate_>* accumulator)
+    {
+        startTime = SystemTime::Now();
+        destination = accumulator;
+    }
+
+    //==============================================================================
+    template <typename Rate_>
+    CScopedTimeAccumulator<Rate_>::~CScopedTimeAccumulator()
+    {
+        auto current = SystemTime::Now();
+        std::chrono::duration<float, Rate_> elapsed = current - startTime;
+
+        *destination += elapsed;
+    }
 }
