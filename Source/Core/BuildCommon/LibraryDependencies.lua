694a19066f5f6ca2aa72cd849d0a958c22f287e1
Joe Schutte <schutte.joe@gmail.com>
2018-08-31 21:48:05 -0700

    Update to latest version of asset importer with local changes to be able to support the osOcean element

--- Source/Core/BuildCommon/LibraryDependencies.lua
+++ Source/Core/BuildCommon/LibraryDependencies.lua
@@ -1,12 +1,12 @@
 
 local platform = ...
 
-includedirs { MiddlewareDir .. "assimp-4.1.0\\include" }
+includedirs { MiddlewareDir .. "assimp-latest\\include" }
 includedirs { MiddlewareDir .. "rapidjson\\include" }
 
 if platform == "Win64" then
-	table.insert(middlewareLinkDirectories, "assimp-4.1.0\\lib\\Release")
-	table.insert(postBuildCopies, "assimp-4.1.0\\bin\\Release\\assimp-vc140-mt.dll $(TargetDir)assimp-vc140-mt.dll")
+	table.insert(middlewareLinkDirectories, "assimp-latest\\lib\\Release")
+	table.insert(postBuildCopies, "assimp-latest\\lib\\Release\\assimp-vc140-mt.dll $(TargetDir)assimp-vc140-mt.dll")
     table.insert(middlewareLibraries, "assimp-vc140-mt")
 elseif platform == "osx" then
 	table.insert(middlewareLinkDirectories, "assimp-osx\\lib")
